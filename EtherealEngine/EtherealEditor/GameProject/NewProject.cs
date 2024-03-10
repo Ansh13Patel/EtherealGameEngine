c560f74a7b3e7c32ae5c8a4f7e5f38ab89e36099
Patel Ansh <anshp4556@gmail.com>
2024-03-10 16:01:48 +0530

    Create new Project window
    Add different template in create new project window and also user can set name and path of project

---
+++ EtherealEngine/EtherealEditor/GameProject/NewProject.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using EtherealEditor.Utilities;
+
+namespace EtherealEditor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<String> Folder { get; set; }
+
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+
+    class NewProject : ViewModelBase
+    {
+        private readonly string _templatePath = @"..\..\EtherealEditor\ProjectTemplates";
+
+        private string _projectname = "New Project";
+        
+        public string ProjectName
+        {
+            get => _projectname;
+
+            set
+            {
+                if(_projectname != value)
+                {
+                    _projectname = value;
+                    OnPropertyChanged(ProjectName);
+                }
+            }
+        }
+
+        private string _projectpath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\EtherealProject\";
+
+        public string ProjectPath
+        {
+            get => _projectpath;
+
+            set
+            {
+                if (_projectpath != value)
+                {
+                    _projectpath = value;
+                    OnPropertyChanged(ProjectPath);
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projecttemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
+        { get; }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projecttemplates);
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+
+                foreach(var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                    
+                    _projecttemplates.Add(template);
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+    }
+}
